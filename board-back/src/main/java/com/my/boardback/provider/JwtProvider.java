f1a65d128b130256c1f5a3593945b51ffa08d27a
JinDalgi <dalgi5986@gmail.com>
2023-12-14 15:38:55 +0900

    Initial commit

---
+++ board-back/src/main/java/com/my/boardback/provider/JwtProvider.java
@@ -0,0 +1,9 @@
+package com.my.boardback.provider;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+    
+    private String secretKey = "SecretK3y";
+}
