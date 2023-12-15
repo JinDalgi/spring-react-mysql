895620a32bdde3988c6ceb77a7d90576f09de568
JinDalgi <dalgi5986@gmail.com>
2023-12-15 16:42:49 +0900

    config

--- board-back/src/main/java/com/my/boardback/config/WebSecurityConfig.java
+++ board-back/src/main/java/com/my/boardback/config/WebSecurityConfig.java
@@ -1,16 +1,24 @@
 package com.my.boardback.config;
 
 import com.my.boardback.filter.JwtAuthenticationFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Configurable
 @EnableWebSecurity
@@ -24,13 +32,15 @@ public class WebSecurityConfig {
 
         httpSecurity.authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/", "/css/**", "/js/**", "/favicon.ico").permitAll()
-                        .requestMatchers("/**", "/").permitAll())
+                        .requestMatchers("/api/v1/auth/**", "/api/v1/search/**", "/file/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/board/**", "/api/v1/user/*").permitAll()
+                        .anyRequest().authenticated())
                 // csrf, formLogin, session 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .headers(headers-> headers
+                .headers(headers -> headers
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))  // X-Frame-Options 헤더 비활성화
 
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
