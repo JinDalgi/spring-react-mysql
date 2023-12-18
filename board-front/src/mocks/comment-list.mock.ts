b2df19c0372ef2545331e4d619f8cda1f074f94e
JinDalgi <dalgi5986@gmail.com>
2023-12-18 15:03:44 +0900

    댓글, 좋아요 컴포넌트

---
+++ board-front/src/mocks/comment-list.mock.ts
@@ -0,0 +1,24 @@
+import {CommentListItem} from "../types/interface";
+
+const commentListMock: CommentListItem [] = [
+    {
+        "nickname": "Test NickName 1",
+        "profileImage": null,
+        "writeDatetime": "3분전",
+        "content": "테스트 content ~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+    },
+    {
+        "nickname": "Test NickName 2",
+        "profileImage": null,
+        "writeDatetime": "3분전",
+        "content": "테스트 content @@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+    },
+    {
+        "nickname": "Test NickName 3",
+        "profileImage": null,
+        "writeDatetime": "3분전",
+        "content": "테스트 content #############################",
+    },
+];
+
+export default commentListMock;
---
+++ board-front/src/mocks/favorite-list.mock.ts
@@ -0,0 +1,40 @@
+const favoriteListMock = [
+    {
+        "email": "test@gmail.com",
+        "nickname": "Test NickName",
+        "profileImage": null
+    },
+    {
+        "email": "test@gmail.com",
+        "nickname": "Test NickName",
+        "profileImage": null
+    },
+    {
+        "email": "test@gmail.com",
+        "nickname": "Test NickName",
+        "profileImage": null
+    },
+    {
+        "email": "test@gmail.com",
+        "nickname": "Test NickName",
+        "profileImage": null
+    },
+    {
+        "email": "test@gmail.com",
+        "nickname": "Test NickName",
+        "profileImage": null
+    },
+    {
+        "email": "test@gmail.com",
+        "nickname": "Test NickName",
+        "profileImage": null
+    },
+    {
+        "email": "test@gmail.com",
+        "nickname": "Test NickName",
+        "profileImage": null
+    },
+
+];
+
+export default favoriteListMock;
