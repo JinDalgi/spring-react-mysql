d5f831ed76532f9aac78490de1ce898b45505e60
JinDalgi <dalgi5986@gmail.com>
2023-12-18 13:17:07 +0900

    Top3 컴포넌트

---
+++ board-front/src/mocks/top-3-board-list.mock.ts
@@ -0,0 +1,30 @@
+import {BoardListItem} from '../types/interface';
+
+const top3BoardList: BoardListItem[] = [
+    {
+        boardNumber: 1,
+        title: "Test1",
+        content: "테스트 게시글 1",
+        boardTitleImage: null,
+        favoriteCount: 11,
+        commentCount: 11,
+        viewCount: 11,
+        writeDatetime: "2023.12.18. 10:17:30",
+        writerNickname: "Test1",
+        writerProfileImage: null,
+    },
+    {
+        boardNumber: 2,
+        title: "Test2",
+        content: "테스트 게시글 2",
+        boardTitleImage: null,
+        favoriteCount: 22,
+        commentCount: 22,
+        viewCount: 22,
+        writeDatetime: "2023.12.18. 10:18:30",
+        writerNickname: "Test2",
+        writerProfileImage: null,
+    },
+]
+
+export default top3BoardList;
